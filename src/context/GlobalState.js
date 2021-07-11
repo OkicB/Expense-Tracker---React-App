f64e8d207090b25010f46fc8eb3181eaeac20d00
Bekir Okić <okicbekir@gmail.com>
2021-07-11 14:14:43 +0200

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,7 +1,6 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
-//objekt za smještanje primjera transakcija da budu kao default
 const initialState = {
     transactions: [
         { id: 1, text: 'Flower', amount: -20 },
@@ -10,15 +9,11 @@ const initialState = {
         { id: 4, text: 'Camera', amount: 150 }
     ]
 }
-//kreiranje contexta i smještanje objekta koji sadrži transakcije
 export const GlobalContext = createContext(initialState);
 
-//provider koji uzima stvari iz app-a (komp) u divu a koji predstavljaju childern
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
-//za use reducer moramo koristiti state i dispatch, kada želimo pozvati reducer akciju moramo koristiti dispatch
 
-    //akcije
     function deleteTransaction(id) {
         dispatch({
             type: 'DELETE_TRANSACTION',
@@ -34,11 +29,10 @@ export const GlobalProvider = ({ children }) => {
     }
 
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions, //pristupamo transakcijskom objektu
+        transactions: state.transactions,
         deleteTransaction,
         addTransaction
     }}>
         {children}
     </GlobalContext.Provider>);
-                    //pruža state i akcije
 }
