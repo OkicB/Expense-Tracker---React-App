64ee3dbb88b16548acc8e07820fb034eab9bca86
Bekir Okić <okicbekir@gmail.com>
2021-07-11 14:11:45 +0200

    Update Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
-                            //destrukcija za prop
     const { deleteTransaction } = useContext(GlobalContext);
 
     const sign = transaction.amount < 0 ? '-' : '+';
@@ -11,6 +10,5 @@ export const Transaction = ({ transaction }) => {
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} class="delete-btn">x</button>
         </li>
-    )//za sign postavlja se math.abs da uvijek bude apsolutna vrijednost da ne bi postavljala dupli znak
-    //za className listu ukoliko je pozitivan da se postavi u plus klasu za css ili minus
+    )
 }
